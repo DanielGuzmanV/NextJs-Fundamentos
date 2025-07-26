3202749bcbcb257cfbe8827f9b0e64fa0d23b216
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-07-25 22:35:38 -0400

    page para cuando no se encuentra una ruta

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function notFound() {
+  return (
+    <div className='border-4 border-amber-400 justify-center text-center items-center m-10'>
+      <h2>Sin salida...</h2>
+
+      <p>Pagina no encontrada, ocurrio un error...</p>
+    </div>
+  )
+};
