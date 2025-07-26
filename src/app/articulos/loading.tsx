d8ef7bc759d65df854bf55c9435d5b9c545f08b7
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-07-25 22:35:15 -0400

    page de loading para articulos

---
+++ src/app/articulos/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Cargando() {
+  return (
+    <div className='justify-center items-center text-center'>
+      Cargando pagina...
+    </div>
+  )
+}
