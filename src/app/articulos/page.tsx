0cc15d8f97b5a732ff718df8319c719d84d21d61
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-04 21:52:06 -0400

    configuracion del page de articulos

--- src/app/articulos/page.tsx
+++ src/app/articulos/page.tsx
@@ -14,7 +14,7 @@ const peticionArticulos = async(): Promise<Articulo[]> => {
     try {
       const response = await fetch('https://fakestoreapi.com/products');
       const data = await response.json();
-      await new Promise((resolve) => setTimeout(resolve, 4000));
+      await new Promise((resolve) => setTimeout(resolve, 800));
       return data;
 
     } catch (error) {
@@ -29,31 +29,31 @@ export default async function Articulos() {
   return (
     <>
       <div className="flex flex-col gap-2 justify-center items-center text-center">
-        <p className="m-4">Pagina de articulos</p>
+        <p className="m-4 text-4xl font-semibold text-gray-800 hover:text-gray-300 transition-colors duration-400">Pagina de articulos</p>
         <Link href="/articulos/vender">
-          <button className="hover:bg-blue-500 bg-blue-700 py-2 px-4 rounded-md w-24">
+          <button className="hover:bg-blue-500 bg-blue-700 py-2 px-4 rounded-md w-24 transition-colors duration-400">
             vender
           </button>
         </Link>
       </div>
 
-      <div className="mx-4 grid grid-cols-3 gap-7">
+      <div className="mx-4 grid grid-cols-3 gap-2">
         {
           articulos.map((values) => {
             return (
               <Link href={`/articulos/${values.id}`} key={values.id}>
                 <div className="my-4 mx-2 bg-amber-200 rounded-lg hover:bg-amber-100">
-                <h2 className="font-bold text-center p-2">{values.title}</h2>
-                <div className="flex p-2 m-4">
-                  <p className="text-xs">{values.description}</p>
-                  <Image className="opacity-90 rounded-3xl"
-                    width={80}
-                    height={50}
-                    src={values.image}
-                    alt={values.title}
-                  />
+                  <h2 className="font-bold text-center p-2">{values.title}</h2>
+                  <div className="flex p-2 m-4">
+                    <p className="text-xs">{values.description}</p>
+                    <Image className="opacity-90 rounded-3xl"
+                      width={80}
+                      height={50}
+                      src={values.image}
+                      alt={values.title}
+                    />
+                  </div>
                 </div>
-              </div>
               </Link>
             )
           })
