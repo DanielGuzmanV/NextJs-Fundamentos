7bc62cf7781fb246c83f0cbc74e639b6ac83bbcc
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-09 15:07:49 -0400

    configuracion de los metodos por id

---
+++ src/app/api/posts/[id]/route.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/app/libs/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+interface DataParams {
+  params: {id: string}
+}
+
+export async function GET({params}: DataParams) {
+  const {id} = await params
+  const post = await prisma.post.findUnique({
+    where: {
+      id:parseInt(id)
+    }
+  })
+  return NextResponse.json(post);
+}
+
+export async function PUT(request: NextRequest, {params}: DataParams) {
+  const {id} = await params;
+  const datos = await request.json();
+  const postUpdate = await prisma.post.update({
+    where: { id: parseInt(id) },
+    data: { ...datos }
+  })
+  return NextResponse.json(postUpdate);
+}
+
+export async function DELETE({params}: DataParams) {
+  const {id} = await params;
+  try {
+    const postDelete = await prisma.post.delete({
+      where: { id: parseInt(id) }
+    });
+    return NextResponse.json(postDelete);
+
+  } catch (error) {
+    return NextResponse.json({
+      message: "Error: " + error
+    });
+  }
+}
