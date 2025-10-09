0e945ed641a04603933a1580724342aa809e9eba
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-09 09:56:42 -0400

    leve correccion de estilo

--- src/app/ingresar/page.tsx
+++ src/app/ingresar/page.tsx
@@ -1,7 +1,7 @@
 export default function Ingresar() {
   return (
-    <div>
-      <p>Pagina para ingresar</p>
+    <div className="items-center justify-center py-20">
+      <p className="text-center">Pagina para ingresar articulos</p>
     </div>
   )
 }
