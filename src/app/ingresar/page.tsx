e18f8bac3ce3a1b6d81a04e6f18c58cf62fe3342
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-10 11:45:21 -0400

    Se mejoro el formulario para poder editar y eliminar

--- src/app/ingresar/page.tsx
+++ src/app/ingresar/page.tsx
@@ -2,7 +2,18 @@
 
 import { useRouter } from "next/navigation";
 
-export default function Ingresar() {
+type Data = {
+  id: number;
+  titulo: string;
+  contenido: string;
+}
+
+interface EditProps  {
+  edit: boolean;
+  post: Data;
+}
+
+export default function Ingresar({edit, post}: EditProps) {
   const router = useRouter();
 
   const enviarForm = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -12,32 +23,91 @@ export default function Ingresar() {
     const titulo = (form.elements.namedItem('titulo') as HTMLInputElement).value;
     const contenido = (form.elements.namedItem('contenido') as HTMLInputElement).value;
     
-    try {
-      const response = await fetch("/api/posts", {
-        method: "POST",
-        body: JSON.stringify({titulo, contenido})
-      });
-      const datos = await response.json();
-
-      router.push("/posts");
-    } catch (error) {
-      console.log("Error: ", error)
+    if(titulo === "" || contenido === ""){
+      return;
     }
+    if(edit) {
+      try {
+        const response = await fetch(`/api/posts/${post.id}`, {
+          method: "PUT",
+          body: JSON.stringify({titulo, contenido}),
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+        const datos = await response.json();
+  
+        router.push("/posts");
+      } catch (error) {
+        console.log("Error: ", error)
+      }
+      
+    }
+    else {
+      try {
+        const response = await fetch("/api/posts", {
+          method: "POST",
+          body: JSON.stringify({titulo, contenido})
+        });
+        const datos = await response.json();
+  
+        router.push("/posts");
+      } catch (error) {
+        console.log("Error: ", error)
+      }
+    }
+  };
+
+  const borrarPost = async() => {
+    const response = await fetch(`/api/posts/${post.id}`, {
+      method: "DELETE",
+    })
+    const data = await response.json();
+    router.push("/posts");
   }
 
   return (
     <div className="items-center justify-center py-20">
       {/* Titulo: */}
       <h1 className="text-2xl font-bold m-5 text-center">
-        Ingresar nuevo post
+        {edit ? "Editar post" : "Ingresar nuevo post"}
       </h1>
 
       {/* Formulario: */}
-      <div className="flex justify-center">
-        <form className="p-5 bg-white text-black w-1/4 rounded-lg" onSubmit={enviarForm}>
-          <input id="titulo" className="w-full mb-3 p-3 rounded-xl border border-gray-400" type="text" placeholder="Ingrese un titulo"/>
-          <textarea id="contenido" className="w-full mb-3 p-3 rounded-xl border border-gray-400"  rows={3} placeholder="Ingres el contenido"></textarea>
-          <input type="submit" className="bg-pink-700 text-white p-2 rounded-xl hover:bg-rose-400" value={"Insertar"}/>
+      <div className="flex justify-center mx-5">
+        <form 
+          className="p-5 bg-white text-black rounded-lg w-xl" 
+          onSubmit={enviarForm}>
+          <input 
+            id="titulo" 
+            className="w-full mb-3 p-3 rounded-xl border border-gray-400" 
+            type="text" 
+            placeholder="Ingrese un titulo"
+            defaultValue={edit ? post.titulo : ""}
+          />
+          <textarea 
+            id="contenido" 
+            className="w-full mb-3 p-3 rounded-xl border border-gray-400" 
+            rows={3}
+            placeholder="Ingres el contenido"
+            defaultValue={edit ? post.contenido : ""}
+          ></textarea>
+          <div className="flex justify-between">
+            <input 
+              type="submit" 
+              className={`${edit ? "bg-green-500 text-white hover:bg-green-400" : "bg-blue-500 text-white hover:bg-blue-400"} p-2 rounded-xl`} 
+              value={edit ? "Editar" : "Insertar"}
+            />
+            {edit && (
+              <button 
+                type="button" 
+                className="bg-red-600 text-white rounded-lg p-2 hover:bg-rose-400"
+                onClick={borrarPost}
+              >
+                Borrar
+              </button>
+            )}
+          </div>
         </form>
 
       </div>
