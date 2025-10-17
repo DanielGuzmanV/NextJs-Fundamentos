c05832b9c16f59746cc4592a9fc5afa2254fc7ed
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-17 11:40:07 -0400

    se agrego una nueva ruta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,26 +29,42 @@ export default function RootLayout(
         <nav>
           <ul className="flex gap-3 justify-center bg-gradient-to-t from-blue-100 to-blue-300 text-black text-xl font-semibold py-5">
             <li>
-              <Link href="/" className="hover:bg-slate-400 hover:text-white rounded-3xl py-2 px-6 transform transition-all">
+              <Link 
+                href="/" 
+                className="hover:bg-slate-400 text-center flex justify-center hover:text-white rounded-3xl py-2 px-6 transform transition-all">
                 Home
               </Link>
             </li>
 
             <li>
-              <Link  href="/ingresar" className="hover:bg-slate-400 hover:text-white rounded-3xl py-2 px-6 transform transition-all">
+              <Link 
+                href="/ingresar" 
+                className="hover:bg-slate-400 text-center flex justify-center hover:text-white rounded-3xl py-2 px-6 transform transition-all">
                 Ingresar
               </Link>
             </li>
 
             <li>
-              <Link href="/articulos" className="hover:bg-slate-400 hover:text-white rounded-3xl py-2 px-6 transform transition-all">
+              <Link 
+                href="/articulos" 
+                className="hover:bg-slate-400 text-center flex justify-center hover:text-white rounded-3xl py-2 px-6 transform transition-all">
                 Articulos
               </Link>
             </li>
 
             <li>
-              <Link href="/posts" className="hover:bg-slate-400 hover:text-white rounded-3xl py-2 px-6 transform transition-all">
+              <Link 
+                href="/posts" 
+                className="hover:bg-slate-400 text-center flex justify-center hover:text-white rounded-3xl py-2 px-6 transform transition-all">
                   Posts
+              </Link>
+            </li>
+
+            <li>
+              <Link 
+                href="/ingresarSA" 
+                className="hover:bg-slate-400 text-center flex justify-center hover:text-white rounded-3xl py-2 px-6 transform transition-all">
+                  Ingresar server action
               </Link>
             </li>
           </ul>
